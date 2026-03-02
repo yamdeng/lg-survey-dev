38cb3ac9bbe2a082e225c6b9656646c5876d07b2
yamdeng <yamdeng@gmail.com>
2026-03-02 13:24:30 +0900

    로케일 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
 import { customTheme } from '@/publish/customTheme';
 import { ConfigProvider } from 'antd';
+import koKR from 'antd/locale/ko_KR';
+import dayjs from 'dayjs';
 import 'dayjs/locale/en';
 import 'dayjs/locale/ko';
 import { setAutoFreeze } from 'immer';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { Router } from '@/routes/MainRouter.tsx';
+
+import advancedFormat from 'dayjs/plugin/advancedFormat';
+import quarterOfYear from 'dayjs/plugin/quarterOfYear';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(quarterOfYear);
+dayjs.extend(advancedFormat);
+dayjs.extend(relativeTime);
+dayjs.locale('ko');
 
 /* aggrind 전역 setting */
 import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
@@ -19,7 +30,7 @@ import './yupLocale';
 setAutoFreeze(false);
 
 createRoot(document.getElementById('root')!).render(
-  <ConfigProvider theme={customTheme}>
+  <ConfigProvider theme={customTheme} locale={koKR}>
     <App>
       <RouterProvider router={Router} />
     </App>
